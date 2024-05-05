69b52c5d1e05955cb7c291908272ed16b6b09e43
AmeenGhouri99 <zoya02820@gmail.com>
2024-05-05 16:23:55 +0500

    works on the choose to apply and documents of frontend

--- resources/views/user/choose_to_apply/create.blade.php
+++ resources/views/user/choose_to_apply/create.blade.php
@@ -31,18 +31,16 @@
                                 <div class="col-xl-4 col-sm-6 col-12 mb-2 mb-xl-0">
                                     <label for="name">Quota/Category Applied</label>
                                     <div class="row">
-                                        <div class="col-8">
+                                        <div class="col-6">
                                             {{ html()->text('category_quota_applied')->class('form-control form-control-sm') }}
                                         </div>
-                                        <div class="col-2">
-                                            <input type="submit
-                                    " value="Select"
-                                                name="select_qouta" class="btn btn-primary btn-sm">
-                                        </div>
-                                        <div class="col-2">
-                                            <input type="submit
-                                    " value="Applied"
-                                                name="applied_to_quota" class="btn btn-success btn-sm">
+                                        <div class="col-6">
+                                            <button type="button" value="Select" name="select_qouta"
+                                                class="btn btn-primary btn-sm"> Select
+                                            </button>
+                                            <button type="button" value="Select" name="applied_to_quota"
+                                                class="btn btn-success btn-sm"> Applied
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\UserAcademicInformationController;
 use App\Http\Controllers\UserChooseProgramController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserDocumentController;
 use App\Http\Controllers\UserPersonalInformationController;
 
 Route::get('/', function () {
@@ -29,4 +30,5 @@ Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
     Route::resource('personal-information', UserPersonalInformationController::class);
     Route::resource('academic-information', UserAcademicInformationController::class);
     Route::resource('choose-program-to-apply', UserChooseProgramController::class);
+    Route::resource('documents', UserDocumentController::class);
 });
